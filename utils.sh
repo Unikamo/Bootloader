dc63d62c6648cf9b09a83e197412f5d884b5b573
Unikamo <foxtrot.4874@gmail.com>
2025-01-09 22:40:39 +0100

    Updated utils.sh

--- utils.sh
+++ utils.sh
@@ -4,20 +4,22 @@ echo "1. Link boot.S && launch QEMU"
 echo "2. Launch QEMU"
 echo "3. (Re)Create virtual disk"
 
-if [ "$1" -eq 1 ]; then
+read answer
+
+if [ "$answer" -eq 1 ]; then
     nasm -f bin -o boot.bin boot.S
     echo "File linked, launching QEMU..."
     qemu-system-i386 \
   -drive format=raw,file=boot.bin,if=floppy \
   -drive format=raw,file=disk.img,if=ide,index=0
     echo "QEMU launched."
-elif [ "$1" -eq 2 ]; then
+elif [ "$answer" -eq 2 ]; then
     qemu-system-i386 \
   -drive format=raw,file=boot.bin,if=floppy \
   -drive format=raw,file=disk.img,if=ide,index=0
 
     echo "QEMU launched."
-elif [ "$1" -eq 3 ]; then
+elif [ "$answer" -eq 3 ]; then
     qemu-img create -f raw disk.img 10M
     echo "Disk (re)created: disk.img"
 else
