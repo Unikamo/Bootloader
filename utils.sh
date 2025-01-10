9fdfcbe733a5ad3b887047501d0128778239c295
Unikamo <foxtrot.4874@gmail.com>
2025-01-10 18:57:46 +0100

    Pain.

--- utils.sh
+++ utils.sh
@@ -7,22 +7,36 @@ echo "3. (Re)Create virtual disk"
 read answer
 
 if [ "$answer" -eq 1 ]; then
-    nasm -f bin -o boot.bin boot.S
-    echo "File linked, launching QEMU..."
-    qemu-system-i386 \
-  -drive format=raw,file=boot.bin,if=floppy \
-  -drive format=raw,file=disk.img,if=ide,index=1 \
-  -boot order=a
-    echo "QEMU launched."
+	nasm -f bin -o boot.bin boot.S
+	echo "File linked, do you want to recreate the virtual disk (y/n) ? (Bootloader won't be updated until you do it)"
+	read disk_answer
+
+	if [[ "$disk_answer" -eq "yes" || "$disk_answer" -eq "y" ]]; then
+		qemu-img create -f raw disk.img 1G
+		dd if=boot.bin of=disk.img bs=512 seek=0
+		echo "Disk (re)created: disk.img"
+	elif [[ "$disk_answer" -eq "no" || "$disk_answer" -eq "n" ]]; then
+		echo "Disk was not re-created."
+	else
+		echo "Answer : \"y/yes || n/no\""
+	fi
+	
+	qemu-system-i386 \
+	-drive format=raw,file=disk.img,if=ide,index=1 \
+	-boot order=c # A && B for Floppy ; C for HD 
+	
+	echo "QEMU launched."
 elif [ "$answer" -eq 2 ]; then
-    qemu-system-i386 \
-  -drive format=raw,file=boot.bin,if=floppy \
-  -drive format=raw,file=disk.img,if=ide,index=1 \
-  -boot order=a 
-    echo "QEMU launched."
+	qemu-system-i386 \
+	-drive format=raw,file=disk.img,if=ide,index=1 \
+	-boot order=c # A && B for Floppy ; C for HD
+	
+	echo "QEMU launched."
 elif [ "$answer" -eq 3 ]; then
-    qemu-img create -f raw disk.img 10M
-    echo "Disk (re)created: disk.img"
+	qemu-img create -f raw disk.img 5G
+	dd if=boot.bin of=disk.img bs=512 seek=0
+	
+	echo "Disk (re)created: disk.img"
 else
-    echo "Usage : \"./utils.sh [CHOICE]\""
+	 echo "Usage : \"./utils.sh [CHOICE]\""
 fi
